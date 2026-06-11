7b6ce74e3c908e890bf469c292ec379348b153f1
naaysha0112 <naayshajain0112@gmail.com>
2026-06-11 13:03:21 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Navbar   from './components/Navbar';
-import Stepper  from './components/Stepper';
+import Navbar from './components/Navbar';
+import Stepper from './components/Stepper';
 
-import UploadJD     from './pages/UploadJD';
-import ReviewJD     from './pages/ReviewJD';
+import UploadJD from './pages/UploadJD';
+import ReviewJD from './pages/ReviewJD';
 import UploadResume from './pages/UploadResume';
-import Report       from './pages/Report';
+import Report from './pages/Report';
 
 export default function App() {
   return (
@@ -15,10 +15,10 @@ export default function App() {
       <Stepper />
 
       <Routes>
-        <Route path="/"              element={<UploadJD />} />
-        <Route path="/review"        element={<ReviewJD />} />
+        <Route path="/" element={<UploadJD />} />
+        <Route path="/review" element={<ReviewJD />} />
         <Route path="/upload-resume" element={<UploadResume />} />
-        <Route path="/report"        element={<Report />} />
+        <Route path="/report" element={<Report />} />
       </Routes>
     </BrowserRouter>
   );
